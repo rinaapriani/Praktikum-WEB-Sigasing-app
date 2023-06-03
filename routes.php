294ea057a0d015e1e16a7a5cb8e9380cf244f0ba
rinaapriani <rinaapriani2001@gmail.com>
2023-06-03 12:21:03 +0800

    Membuat Halaman Rekapitulasi Penggajian

--- routes.php
+++ routes.php
@@ -62,6 +62,9 @@ if (isset($_GET['page'])) {
                 break;  
         case 'karyawanjabatan':
             file_exists('pages/admin/karyawanjabatan.php') ? include 'pages/admin/karyawanjabatan.php' : include "pages/404.php";
+                break; 
+        case 'penggajianrekap':
+            file_exists('pages/admin/penggajianrekap.php') ? include 'pages/admin/penggajianrekap.php' : include "pages/404.php";
                 break;    
         default:
             include "pages/404.php";
